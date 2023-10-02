e9144de5ebc8f10ea4e2079bf05310e458322fcc
Cintia Lopes <135193611+cindyyershees@users.noreply.github.com>
2023-10-02 16:23:36 -0300

    Create colecoes.py

---
+++ colecoes.py
@@ -0,0 +1,12 @@
+"""
+LIST= (lista= array)
+Itens da lista sao ordenadas,mutáveis e permitem valores duplicados .
+"""
+# Indices-> [0] [1] [2] [3] [4]
+aluno = ["Juan", "Marcia","Pedro", "Israel","Marcia"]
+print(aluno)
+print(len(aluno)) #len = length = tamanho
+aluno[2] = "tatatá"
+print(aluno)
+cliente = ['juan',56.78, True,4] #aceita,mútiplos tipos
+print(cliente)
