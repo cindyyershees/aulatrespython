43e6ec663b5caa9719d43eb3101702b348ab113e
Cintia Lopes <135193611+cindyyershees@users.noreply.github.com>
2023-10-02 16:26:38 -0300

    Create colecoestuplas.py

---
+++ colecoestuplas.py
@@ -0,0 +1,9 @@
+"""
+Lista é uma coleção ordenada e mutável , permite membros duplicados , utiliza-se [] colchetes
+Tupla é uma coleção ordenada e imutável, permite menbros duplicados , utiliza-se () parenteses
+Tupla sempre terá tamanho fixo, constante.
+"""
+minhaTupla = ("maçã","banana","Abacaxi","maçã")
+print(minhaTupla)
+#minhaTupla[2] ="kiwi"
+#print(minhaTupla) #dá erropois a tupla não permite trocar valor
